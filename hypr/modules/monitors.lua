9e96ad874d63e18914b14e1b4e0c0a049bf51954
Neryss <neryssistos@gmail.com>
2026-06-07 16:53:42 +0200

    added: nvim

--- hypr/modules/monitors.lua
+++ hypr/modules/monitors.lua
@@ -3,7 +3,7 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
     output		= "DP-1",
-    mode		= "1920x1080@165",
+    mode		= "1920x1080@240",
     position	= "0x0",
     scale		= "1",
 })
